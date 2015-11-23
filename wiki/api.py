ed3f04a489dfc6132d16f75ca6ac3ab43a8016da
thoma5B <thomas.bunke@gmail.com>
2015-11-23 22:22:16 +0100

    bugfix translations

--- wiki/api.py
+++ wiki/api.py
@@ -6,16 +6,15 @@ from django.contrib.auth.models import User
 from tastypie import fields
 
 
-
 class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
         # resource_name = 'user'
         # one of the following two
         # fields = ['username', 'first_name', 'last_name', 'last_login']
-        # excludes = ['email', 'password', 'is_active', 'is_staff', 'is_superuser']
+        # excludes = ['email', 'password', 'is_active',
+        #               'is_staff', 'is_superuser']
         allowed_methods = ['get']
-
 
 
 class ArticleResource(ModelResource):
@@ -24,9 +23,10 @@ class ArticleResource(ModelResource):
         # resource_name = 'article' # this is the default resource name
         authorization = Authorization()
 
+
 class ArticleRevisionResource(ModelResource):
     class Meta:
-        queryset =  ArticleRevision.objects.all()
+        queryset = ArticleRevision.objects.all()
         resource_name = 'content'
-        fields = ['content','title']
+        fields = ['content', 'title']
         authorization = Authorization()
